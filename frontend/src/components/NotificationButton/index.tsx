8aa7c370148dd28742aadee711f42da62ef7827b
SandroJAM <sandrojam@hotmail.com>
2022-07-15 17:13:18 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg'
 import { BASE_URL } from '../../utils/request';
 
@@ -13,8 +14,8 @@ function handleClick(id : number) {
 
   axios(`${BASE_URL}/sales/${id}/notification`)
        .then(response => {
-            console.log("SUCESSO");
-       })
+           toast.info("SMS enviado com SUCESSO!"); //console.log("SUCESSO")
+       });
 }
 
 // function NotificationButton() {
